11ab45c8b21bcae3fd87a4f68e66ea670ccff888
amanharsora5403 <amanharsora543@gmail.com>
2024-12-30 16:20:57 +0530

    fourth change

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  /*
+  access_key = "AKIAZOZQF5TOAHUNJX6N"
+  secret_key = "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"
+  */
+}
