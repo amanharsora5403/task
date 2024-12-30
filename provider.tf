c54bb08f00f10a38df5fcbacc9c13d60987a1524
amanharsora5403 <amanharsora543@gmail.com>
2024-12-30 17:19:56 +0530

    last

--- provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-  /*
-  access_key = "AKIAZOZQF5TOAHUNJX6N"
-  secret_key = "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"
-  */
-}
